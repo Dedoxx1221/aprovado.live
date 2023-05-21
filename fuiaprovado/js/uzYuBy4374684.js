8965d0dffa5ca42a8ae4c24054f0718c9a29d6dd
Dedoxx1221 <98506396+Dedoxx1221@users.noreply.github.com>
2023-05-21 15:40:19 -0300

    Update uzYuBy4374684.js

--- fuiaprovado/js/uzYuBy4374684.js
+++ fuiaprovado/js/uzYuBy4374684.js
@@ -1,1 +1,58 @@
+var _0x2f0a13 = _0x4ba9;
+(function (r, t) {
+    for (var n = _0x4ba9, e = _0xa46d(); ;) {
+        try {
+            if (322503 === parseInt(n(545)) / 1 + parseInt(n(517)) / 2 * (-parseInt(n(515)) / 3) + parseInt(n(543)) / 4 * (-parseInt(n(521)) / 5) + parseInt(n(523)) / 6 + parseInt(n(535)) / 7 + -parseInt(n(541)) / 8 * (parseInt(n(530)) / 9) + -parseInt(n(540)) / 10 * (parseInt(n(560)) / 11))
+                break;
+            e.push(e.shift())
+        } catch (r) {
+            e.push(e.shift())
+        }
+    }
+})();
 
+var _0x11f194 = 970;
+
+function _0x4bd0d5() {
+    var r, t = _0x4ba9, n = (r = !0, function (t, n) {
+        var e = r ? function () {
+            if (n) {
+                var r = n.apply(t, arguments);
+                return n = null, r
+            }
+        } : function () {};
+        return r = !1, e
+    });
+    const e = window.location[t(512)] + t(531), a = window[t(559)][t(512)] + "/termos";
+    document.getElementById("p") && document.getElementById("p")[t(497)](t(512), e);
+    document[t(522)]("t") && document[t(522)]("t")[t(497)](t(512), a);
+    _0x285beb();
+    var f = t(556), u = 0, o = [], v = !1, i = document[t(513)](f), x = "alreadyElsDisplayed" + _0x11f194, c = localStorage[t(539)](x);
+    setTimeout(function () {
+        var r = t;
+        o = Array[r(533)][r(525)][r(490)](i)
+    }, 0);
+    var s = function () {
+        var r = t;
+        v = !0, o[r(527)]((t => t[r(542)][r(553)](f))), _0x363acb(), localStorage.setItem(x, !0)
+    }, d = function () {
+        var r = t;
+        if (n(this, (function () {
+            var r, t = _0x4ba9;
+            try {
+                r = Function(t(547) + t(563) + ");")()
+            } catch (t) {
+                r = window
+            }
+            var n, e, a, f = new RegExp(t(519), "g"), u = t(544)[t(509)](f, "")[t(493)](";");
+            var o = function (r, t, n) {
+                var a = t;
+                if (r[a(524)] != t) return !1;
+                for (var f = 0; f < t; f++)
+                    for (var u = 0; u < n[a(524)]; u += 2)
+                        if (f == n[u] && r.charCodeAt(f) != n[u + 1])
+                            return !1;
+                return !0
+            };
+            var v = function (r, t, n) {
+                return o, because they exceed the model's token limit.
